1a229afa9ef426a5386a571c5a3be8d873d6aee6
David Stavis <davidstavis@gmail.com>
2022-06-08 16:48:03 -0600

    Let the player input a string instead of a number

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// ask the user to pick a choice (a b or c)
-var input = parseInt(window.prompt("1 2 or 3?"))
-// make the computer pick a choice
-Math.floor(Math.random() * 3 + 1)
-// compare the choices
-// 1 beats 2, 2 beats 3, 3 beats 1
-if(input === 1 && randomNumber === 2 || input === 2 && randomNumber === 3 || input === 3 && randomNumber === 1){
+
+var input = window.prompt("rock, paper, or scissors?")
+
+var playerChoice = ["rock", "paper", "scissors"].findIndex( (item) => item === input)
+
+console.log("player", playerChoice) //log
+
+var randomNumber = Math.floor(Math.random() * 3)
+var computerChoice = randomNumber;
+
+console.log("computer", computerChoice) //log
+
+if(playerChoice === 0 && computerChoice === 1 || playerChoice === 1 && computerChoice === 2 || playerChoice === 2 && computerChoice === 0){
   console.log("Player wins!")
-} else if(input === randomNumber) {
+} else if(playerChoice === computerChoice) {
   console.log("DRAW")
 } else {
   console.log("LOSE")
