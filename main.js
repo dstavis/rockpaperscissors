58bb197115a994ba8d40693ca12ceb0e1980acb3
David Stavis <davidstavis@gmail.com>
2022-06-08 16:57:35 -0600

    Replace prompt with button inputs

--- main.js
+++ main.js
@@ -1,19 +1,35 @@
+// var input = window.prompt("rock, paper, or scissors?")
 
-var input = window.prompt("rock, paper, or scissors?")
+var playerChoice;
+var option1 = document.getElementById("0")
+var option2 = document.getElementById("1")
+var option3 = document.getElementById("2")
 
-var playerChoice = ["rock", "paper", "scissors"].findIndex( (item) => item === input)
+option1.addEventListener("click", buttonHandler)
+option2.addEventListener("click", buttonHandler)
+option3.addEventListener("click", buttonHandler)
 
-console.log("player", playerChoice) //log
+function buttonHandler(eventObject){
+  playerChoice = parseInt(event.target.id)
+  run()
+}
 
-var randomNumber = Math.floor(Math.random() * 3)
-var computerChoice = randomNumber;
+function run(){
+  // var playerChoice = ["rock", "paper", "scissors"].findIndex( (item) =>
+  //  item === input)
 
-console.log("computer", computerChoice) //log
+  console.log("player", playerChoice) //log
 
-if(playerChoice === 0 && computerChoice === 1 || playerChoice === 1 && computerChoice === 2 || playerChoice === 2 && computerChoice === 0){
-  console.log("Player wins!")
-} else if(playerChoice === computerChoice) {
-  console.log("DRAW")
-} else {
-  console.log("LOSE")
+  var randomNumber = Math.floor(Math.random() * 3)
+  var computerChoice = randomNumber;
+
+  console.log("computer", computerChoice) //log
+
+  if(playerChoice === 0 && computerChoice === 1 || playerChoice === 1 && computerChoice === 2 || playerChoice === 2 && computerChoice === 0){
+    console.log("WIN")
+  } else if(playerChoice === computerChoice) {
+    console.log("DRAW")
+  } else {
+    console.log("LOSE")
+  }
 }
