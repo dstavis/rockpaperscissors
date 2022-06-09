c9cfadab8af453f5b5251b4b6218b7d02ebca28d
David Stavis <davidstavis@gmail.com>
2022-06-09 10:47:21 -0600

    Make the choice labels dynamically swappable

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-// var input = window.prompt("rock, paper, or scissors?")
+
+var labelOptions =
+{
+  normal: ["rock", "paper", "scissors"],
+  pokemon: ["charmander", "bulbasaur", "squirtle"],
+  rpg: ["boulder", "parchment", "shears"],
+}
+const capitalize = ([ firstLetter, ...remainingLetters ]) => {
+  return firstLetter.toUpperCase() + remainingLetters.join('')
+}
+var currentLabels = labelOptions.pokemon
+currentLabels = currentLabels.map( (label) => capitalize(label) )
+
 
 var playerChoice;
 var option1 = document.getElementById("0")
 var option2 = document.getElementById("1")
 var option3 = document.getElementById("2")
+
+option1.innerText = currentLabels[0]
+option2.innerText = currentLabels[1]
+option3.innerText = currentLabels[2]
 
 option1.addEventListener("click", buttonHandler)
 option2.addEventListener("click", buttonHandler)
@@ -15,15 +31,12 @@ function buttonHandler(eventObject){
 }
 
 function run(){
-  // var playerChoice = ["rock", "paper", "scissors"].findIndex( (item) =>
-  //  item === input)
-
-  console.log("player", playerChoice) //log
+  console.log("player", currentLabels[playerChoice]) //log
 
   var randomNumber = Math.floor(Math.random() * 3)
   var computerChoice = randomNumber;
 
-  console.log("computer", computerChoice) //log
+  console.log("computer", currentLabels[computerChoice]) //log
 
   if(playerChoice === 0 && computerChoice === 1 || playerChoice === 1 && computerChoice === 2 || playerChoice === 2 && computerChoice === 0){
     console.log("WIN")
