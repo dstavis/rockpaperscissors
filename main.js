618b7ec5b0b70580c2babb0f994139b1178e7fdc
David Stavis <davidstavis@gmail.com>
2022-06-13 12:47:09 -0600

    Working 5p algo in the console

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 var labelOptions =
 {
   normal: ["rock", "paper", "scissors"],
-  pokemon: ["charmander", "bulbasaur", "squirtle"],
+  pokemon: ["bulbasaur", "charmander", "squirtle", "pikachu", "geodude"],
   rpg: ["boulder", "parchment", "shears"],
 }
 const capitalize = ([ firstLetter, ...remainingLetters ]) => {
@@ -13,31 +13,72 @@ currentLabels = currentLabels.map( (label) => capitalize(label) )
 
 
 var playerChoice;
-var option1 = document.getElementById("0")
-var option2 = document.getElementById("1")
-var option3 = document.getElementById("2")
+var option1 = document.getElementById("1")
+var option2 = document.getElementById("2")
+var option3 = document.getElementById("3")
+var option4 = document.getElementById("4")
+var option5 = document.getElementById("5")
 
 option1.innerText = currentLabels[0]
 option2.innerText = currentLabels[1]
 option3.innerText = currentLabels[2]
+option4.innerText = currentLabels[3]
+option5.innerText = currentLabels[4]
 
 option1.addEventListener("click", buttonHandler)
 option2.addEventListener("click", buttonHandler)
 option3.addEventListener("click", buttonHandler)
+option3.addEventListener("click", buttonHandler)
+option4.addEventListener("click", buttonHandler)
+option5.addEventListener("click", buttonHandler)
 
 function buttonHandler(eventObject){
-  playerChoice = parseInt(event.target.id)
+  playerChoice = parseInt(event.target.id) // TODO: put back when done testing
+  // playerChoice = 1;
   run()
 }
 
 function run(){
-  console.log("player", currentLabels[playerChoice]) //log
+  console.log("player", currentLabels[playerChoice - 1]) //log
 
-  var randomNumber = Math.floor(Math.random() * 3)
+  var randomNumber = Math.floor((Math.random() * 5) + 1)
   var computerChoice = randomNumber;
 
-  console.log("computer", currentLabels[computerChoice]) //log
+  console.log("computer", currentLabels[computerChoice - 1]) //log
 
+  if(playerChoice === computerChoice){
+    console.log("=DRAW=")
+  } else {
+    var winner = determineWinner(playerChoice, computerChoice)
+  }
+
+  if(winner === "player"){
+    console.log("+WIN+")
+  } else if (winner === "computer"){
+    console.log("-LOSE-")
+  }
+
+  winner = undefined;
+}
+
+function determineWinner(playerChoice, computerChoice){
+
+  if(bothEven(playerChoice, computerChoice) || bothOdd(playerChoice, computerChoice)){
+    return playerChoice < computerChoice ? "player" : "computer"
+  } else {
+    return playerChoice > computerChoice ? "player" : "computer"
+  }
+}
+
+function bothEven(numberA, numberB){
+  return numberA % 2 === 0 && numberB % 2 === 0
+}
+
+function bothOdd(numberA, numberB){
+  return numberA % 2 !== 0 && numberB % 2 !== 0
+}
+
+function oldLogic(){
   if(playerChoice === 0 && computerChoice === 1 || playerChoice === 1 && computerChoice === 2 || playerChoice === 2 && computerChoice === 0){
     console.log("WIN")
   } else if(playerChoice === computerChoice) {
